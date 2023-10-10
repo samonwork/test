6d881998dd24ac9fdbe957cd4406ca730fbeb984
Simon Norenius <simon.norenius@unicus.com>
2023-10-10 15:41:33 +0200

    uppladdning

--- pyproj/pythonproject.py
+++ pyproj/pythonproject.py
@@ -2,9 +2,11 @@ import datetime
 stringnr = input("Enter year of birth: ")
 
 try:
-    if int(stringnr) >= 1900:
+    if int(stringnr) >= 1900 and int(stringnr) <= datetime.date.today().year:
         print(f'You were born {datetime.date.today().year - int(stringnr)} years ago.')
-    else:
+    elif int(stringnr) > datetime.date.today().year:
+        print("You're unborn!")
+    else: 
         print("You're dead!")
 
 except Exception:
