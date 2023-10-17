5bd910e3d7c163c017d9ba2bc1c108be908c6772
Simon Norenius <simon.norenius@unicus.com>
2023-10-17 15:43:15 +0200

    update

--- pyproj/pythonproject.py
+++ pyproj/pythonproject.py
@@ -1,6 +1,20 @@
-import datetime
-stringnr = input("Enter year of birth: ")
+import random
 
+secret_number = random.randint(1, 9)
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print("You lost!")
+
+"""
+stringnr = input("Enter year of birth: ")
 try:
     if int(stringnr) >= 1900 and int(stringnr) <= datetime.date.today().year:
         print(f'You were born {datetime.date.today().year - int(stringnr)} years ago.')
@@ -10,4 +24,6 @@ try:
         print("You're dead!")
 
 except Exception:
-    print("Invalid value!")
+    print("Invalid input!")
+
+"""
